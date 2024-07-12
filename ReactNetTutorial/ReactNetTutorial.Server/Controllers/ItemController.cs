a627869cab280688f1bf7f5a4ca0198cf57a469d
Valentina Banner <78287030+realhuman101@users.noreply.github.com>
2024-07-12 14:28:39 +0800

    Add base files

---
+++ ReactNetTutorial/ReactNetTutorial.Server/Controllers/ItemController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ReactNetTutorial.Server.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ItemController : ControllerBase
+    {
+    }
+}
